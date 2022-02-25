7aebdb1dd5e7f36a6fecc7ad70d8c28b679c437f
tovehydmark <tovehydmark@gmail.com>
2022-02-25 15:33:01 +0100

    initial commit - all starting components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StartComponent } from './components/start/start.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ProductComponent } from './components/product/product.component';
+import { AddProductComponent } from './components/add-product/add-product.component';
+import { BasketComponent } from './components/basket/basket.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CheckOutComponent } from './components/check-out/check-out.component';
+import { AdminComponent } from './components/admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartComponent,
+    ProductsComponent,
+    ProductComponent,
+    AddProductComponent,
+    BasketComponent,
+    HeaderComponent,
+    FooterComponent,
+    CheckOutComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
